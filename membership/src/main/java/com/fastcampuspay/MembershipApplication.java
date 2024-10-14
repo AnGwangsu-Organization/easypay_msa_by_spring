3660bd558cfe0aace42d301641d6d5a711a3e517
sqs-gwangsu <149981936+sqs-gwangsu@users.noreply.github.com>
2024-10-14 14:55:19 +0900

    Feat: application 세팅

--- membership/src/main/java/com/fastcampuspay/MembershipApplication.java
+++ membership/src/main/java/com/fastcampuspay/MembershipApplication.java
@@ -1,10 +1,11 @@
 package com.fastcampuspay;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MembershipApplication {
     public static void main(String[] args)  {
-        System.out.println("Hello world!");
+        SpringApplication.run(MembershipApplication.class, args);
     }
 }
